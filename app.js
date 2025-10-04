48b03cf56fb9b4b16932d5b4ff91278d2a936695
amirAghayari <amiraghayari2119@gmail.com>
2025-10-04 12:34:05 +0330

    Update app.js , fix user router require errors

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require("express")
-const morgan = require("morgan")
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const AppError = require("./utils/appError")
-
+const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express()
 
-app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json({limit : "30kb"}))
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.get("/" , (req ,res) =>{
+    res.status(200).json({
+        status:"success",
+        message : "welcome to worldWise server api"
+    })
+})
+
+app.use('/api/v1/users', userRouter);
+
+app.use((req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+app.use(globalErrorHandler);
 
 module.exports = app;
